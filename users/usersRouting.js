a1df730df183478f65a85c5fe0d57ea5bd6295f8
gowthamrn4 <gowthamrn4@gmail.com>
2019-01-11 12:39:47 +0530

    api

--- users/usersRouting.js
+++ users/usersRouting.js
@@ -4,12 +4,12 @@ var usersController=require('./usersController');
 
 var usersRouting=express.Router();
 
-// usersRouting.route('/getusers').get(usersController.getusers);
-// usersRouting.route('/adduser').post(usersController.adduser);
-// usersRouting.route('/login').post(usersController.login);
-// usersRouting.route('/finduser').post(usersController.finduser);
-// usersRouting.route('/delusers').post(usersController.deleteuser);
-// usersRouting.route('/setoffline').post(usersController.updateOffline);
+usersRouting.route('/getusers').get(usersController.getusers);
+usersRouting.route('/adduser').post(usersController.adduser);
+usersRouting.route('/login').post(usersController.login);
+usersRouting.route('/finduser').post(usersController.finduser);
+usersRouting.route('/delusers').post(usersController.deleteuser);
+usersRouting.route('/setoffline').post(usersController.updateOffline);
 usersRouting.route('/findOnlieUser').post(usersController.findOnline);
 
 module.exports=usersRouting;
